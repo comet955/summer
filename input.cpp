b0a7497475f0ca917ab8fa5212d3471e8e15e844
comet955 <51965189+comet955@users.noreply.github.com>
2019-06-28 12:05:01 -0700

    Create input.cpp

---
+++ input.cpp
@@ -0,0 +1,15 @@
+// i/o example
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+ int i;
+ cout << "Please enter an integer value: ";
+ cin >> i;
+ cout << "The value you entered is " <<i;
+ cout << " and its trippled is " << i*3 << ".\n";
+ cout<< "and to the 7th power "<< i*i*i*i*i*i*i<<endl;
+ return 0;
+ }
