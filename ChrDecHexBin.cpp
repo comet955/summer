f1a765aaee4f4f0bfc7f11d887f8a33a2b8bd17e
comet955 <51965189+comet955@users.noreply.github.com>
2019-06-28 11:58:08 -0700

    Create ChrDecHexBin.cpp

---
+++ ChrDecHexBin.cpp
@@ -0,0 +1,15 @@
+
+// Decimal to char to (Hex to Bin)
+// (Hex to Bin) is advanced
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+int i;
+for (i = 0; i < 256 ; i++) {
+ cout<<1<<" "<<char(i)<<endl;
+ }
+   return 0;
+   }
