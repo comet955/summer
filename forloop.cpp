697dc5a3c035adf3548ec4997b635d7a1dfcd659
comet955 <51965189+comet955@users.noreply.github.com>
2019-06-28 11:51:57 -0700

    Create forloop.cpp

---
+++ forloop.cpp
@@ -0,0 +1,17 @@
+//hello.cpp kie
+// $g++ hello2.cpp -o hello2.o
+#includ <iostream>
+using namespace std;
+
+int main(){
+int i'j;
+for (;;){
+cout<<i<<" "<<j<<" ";
+if (i % 5== 0)cout<<endl;
+i=i + 1
+j++;
+if j(j > 1000000) break;
+}
+   return 0;
+  
+}
